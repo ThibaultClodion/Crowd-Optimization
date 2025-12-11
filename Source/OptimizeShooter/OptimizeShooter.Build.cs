80913a03d2cd3a978b550fd6742d0444ec0ec8e4
Thibault Clodion <thibaultclodion@gmail.com>
2025-12-11 09:39:23 +0100

    Add NavigationSystem to public dependencies

--- Source/OptimizeShooter/OptimizeShooter.Build.cs
+++ Source/OptimizeShooter/OptimizeShooter.Build.cs
@@ -8,7 +8,7 @@ public class OptimizeShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" , "AIModule", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" , "AIModule", "Niagara", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
